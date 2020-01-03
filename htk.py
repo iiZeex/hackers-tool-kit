5621cdf94746c0a40ad19ec02870db9015afefe3
iiZeex <59490230+iiZeex@users.noreply.github.com>
2020-01-04 00:58:05 +0200

    Update htk.py

--- htk.py
+++ htk.py
@@ -37,104 +37,104 @@ win.mainloop()
 #this is the loading screen \ the imports START#
 #----------------------------------------------#
 import os
-print "Starting hackers-tool-kit...  [ * ]-[0%]"
+print ("Starting hackers-tool-kit...  [ * ]-[0%]"
 import platform
 os.system("clear")
-print "Starting hackers-tool-kit...  [  *]-[5%]"
+print ("Starting hackers-tool-kit...  [  *]-[5%]"
 import webbrowser
 os.system("clear")
-print "Starting hackers-tool-kit...  [ * ]-[9%]"
+print ("Starting hackers-tool-kit...  [ * ]-[9%]"
 import hashlib
 os.system("clear")
-print "Starting hackers-tool-kit...  [*  ]-[14%]"
+print ("Starting hackers-tool-kit...  [*  ]-[14%]"
 import subprocess
 os.system("clear")
-print "Starting hackers-tool-kit...  [ * ]-[19%]"
+print ("Starting hackers-tool-kit...  [ * ]-[19%]"
 import zipfile
 os.system("clear")
-print "Starting hackers-tool-kit...  [  *]-[23%]"
+print ("Starting hackers-tool-kit...  [  *]-[23%]"
 import colorama
 os.system("clear")
-print "Starting hackers-tool-kit...  [ * ]-[25%]"
+print ("Starting hackers-tool-kit...  [ * ]-[25%]"
 from modules import *
 os.system("clear")
-print "Starting hackers-tool-kit...  [*  ]-[26%]"
+print ("Starting hackers-tool-kit...  [*  ]-[26%]"
 import modules.colors
 os.system("clear")
-print "Starting hackers-tool-kit...  [ * ]-[27%]"
+print ("Starting hackers-tool-kit...  [ * ]-[27%]"
 import builtwith
 os.system("clear")
-print "Starting hackers-tool-kit...  [  *]-[29%]"
+print ("Starting hackers-tool-kit...  [  *]-[29%]"
 from urllib2 import urlopen
 os.system("clear")
-print "Starting hackers-tool-kit...  [ * ]-[31%]"
+print ("Starting hackers-tool-kit...  [ * ]-[31%]"
 from urllib2 import URLError
 os.system("clear")
-print "Starting hackers-tool-kit...  [*  ]-[34%]"
+print ("Starting hackers-tool-kit...  [*  ]-[34%]"
 from urllib2 import HTTPError
 os.system("clear")
-print "Starting hackers-tool-kit...  [ * ]-[38%]"
+print ("Starting hackers-tool-kit...  [ * ]-[38%]"
 from urllib import urlencode
 os.system("clear")
-print "Starting hackers-tool-kit...  [*  ]-[39%]"
+print ("Starting hackers-tool-kit...  [*  ]-[39%]"
 from plugins.DNSDumpsterAPI import DNSDumpsterAPI
 os.system("clear")
-print "Starting hackers-tool-kit...  [ * ]-[41%]"
+print ("Starting hackers-tool-kit...  [ * ]-[41%]"
 import whois
 os.system("clear")
-print "Starting hackers-tool-kit...  [  *]-[45%]"
+print ("Starting hackers-tool-kit...  [  *]-[45%]"
 import json
 os.system("clear")
-print "Starting hackers-tool-kit...  [ * ]-[47%]"
+print ("Starting hackers-tool-kit...  [ * ]-[47%]"
 from urlparse import urlparse
 os.system("clear")
-print "Starting hackers-tool-kit...  [*  ]-[50%]"
+print ("Starting hackers-tool-kit...  [*  ]-[50%]"
 from re import search, sub
 os.system("clear")
-print "Starting hackers-tool-kit...  [ * ]-[55%]"
+print ("Starting hackers-tool-kit...  [ * ]-[55%]"
 import cookielib
 os.system("clear")
-print "Starting hackers-tool-kit...  [  *]-[56%]"
+print ("Starting hackers-tool-kit...  [  *]-[56%]"
 import socket
 os.system("clear")
-print "Starting hackers-tool-kit...  [ * ]-[59%]"
+print ("Starting hackers-tool-kit...  [ * ]-[59%]"
 from scapy.all import *
 os.system("clear")
-print "Starting hackers-tool-kit...  [*  ]-[60%]"
+print ("Starting hackers-tool-kit...  [*  ]-[60%]"
 from threading import Thread, active_count
 os.system("clear")
-print "Starting hackers-tool-kit...  [ * ]-[63%]"
+print ("Starting hackers-tool-kit...  [ * ]-[63%]"
 import random
 import readline
 os.system("clear")
-print "Starting hackers-tool-kit...  [  *]-[67%]"
+print ("Starting hackers-tool-kit...  [  *]-[67%]"
 import string
 os.system("clear")
-print "Starting hackers-tool-kit...  [ * ]-[70%]"
+print ("Starting hackers-tool-kit...  [ * ]-[70%]"
 import signal
 os.system("clear")
-print "Starting hackers-tool-kit...  [*  ]-[73%]"
+print ("Starting hackers-tool-kit...  [*  ]-[73%]"
 import ssl  
 os.system("clear")
-print "Starting hackers-tool-kit...  [ * ]-[79%]"
+print ("Starting hackers-tool-kit...  [ * ]-[79%]"
 import argparse
 os.system("clear")
-print "Starting hackers-tool-kit...  [  *]-[83%]"
+print ("Starting hackers-tool-kit...  [  *]-[83%]"
 import sys
 os.system("clear")
-print "Starting hackers-tool-kit...  [ * ]-[86%]"
+print ("Starting hackers-tool-kit...  [ * ]-[86%]"
 import socks
 os.system("clear")
-print "Starting hackers-tool-kit...  [*  ]-[89%]"
+print ("Starting hackers-tool-kit...  [*  ]-[89%]"
 import mechanize
 os.system("clear")
-print "Starting hackers-tool-kit...  [ * ]-[90%]"
+print ("Starting hackers-tool-kit...  [ * ]-[90%]"
 import requests
 os.system("clear")
-print "Starting hackers-tool-kit...  [  *]-[94%]"
+print ("Starting hackers-tool-kit...  [  *]-[94%]"
 import time
 os.system("clear")
-print "Starting hackers-tool-kit...  [ * ]-[96%]"
+print ("Starting hackers-tool-kit...  [ * ]-[96%]"
 from datetime import datetime
 now = datetime.now()
 hour = now.hour
